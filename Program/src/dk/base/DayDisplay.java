2ca3c6c666f7b63458f4ce3c25f711789a9b664e
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-05-14 23:07:22 +0200

    Worked on basic UI

--- Program/src/dk/base/DayDisplay.java
+++ Program/src/dk/base/DayDisplay.java
@@ -1,7 +1,9 @@
 package dk.base;
 
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class DayDisplay extends VBox {
 
@@ -10,5 +12,25 @@ public class DayDisplay extends VBox {
     public DayDisplay(String displayName) {
         super();
         this.displayName = displayName;
+        this.setStyle(  "-fx-padding: 3;" + //Inside: space between border and content
+                        "-fx-border-style: solid inside;" +
+                        "-fx-border-width: 3;" +
+                        "-fx-border-insets: 2;" + //Outside: space to other elements
+                        "-fx-border-radius: 5;" + //Side of rounding in border
+                        "-fx-border-color: black;");
+        this.setMinSize(240, 240);
+
+        Text title = new Text(displayName);
+        HBox header = new HBox();
+        //header.setPadding(new Insets(2));
+        header.setBackground(new Background(new BackgroundFill(new Color(198/255f, 255/255f, 163/255f, 1), null, null)));
+        header.setBorder(new Border(new BorderStroke(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK,
+                BorderStrokeStyle.NONE, BorderStrokeStyle.NONE, BorderStrokeStyle.SOLID, BorderStrokeStyle.NONE,
+                CornerRadii.EMPTY, new BorderWidths(2), Insets.EMPTY)));
+        header.getChildren().add(title);
+
+
+        getChildren().add(header);
+
     }
 }
