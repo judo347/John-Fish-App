b169e5ff1407babed6df316d98c9c598d3849cc9
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-05-14 14:40:24 +0200

    base code extended

---
+++ Program/src/dk/base/App.java
@@ -0,0 +1,32 @@
+package dk.base;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        //Create root
+        Pane root = new HBox();
+
+        //Create days
+        DayDisplay old = new DayDisplay("Old");
+        DayDisplay today = new DayDisplay("Today");
+        DayDisplay tomorrow = new DayDisplay("Tomorrow");
+        DayDisplay theDayAfter = new DayDisplay("The Day After");
+
+        //Add the HBox' to the root
+        root.getChildren().addAll(old, today, tomorrow, theDayAfter);
+
+        //Set the stage
+        Scene scene = new Scene(root, 1280, 620);
+        stage.setScene(scene);
+        stage.setTitle("John Fish App.");
+        stage.show();
+    }
+}
---
+++ Program/src/dk/base/DayDisplay.java
@@ -0,0 +1,14 @@
+package dk.base;
+
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+
+public class DayDisplay extends VBox {
+
+    private String displayName;
+
+    public DayDisplay(String displayName) {
+        super();
+        this.displayName = displayName;
+    }
+}
